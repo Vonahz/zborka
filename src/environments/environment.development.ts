1d25ac4339ce519eafa7dbb8362899d5735329ff
Milen Karmidzhanov <karmidzhanov@gmail.com>
2024-08-23 23:31:04 +0300

    Fix build

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,14 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyC61BeqwPbtOcofGb1vHvvClo1BaS_rNuY",
-        authDomain: "zborkanet.firebaseapp.com",
-        databaseURL: "https://zborkanet-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "zborkanet",
-        storageBucket: "zborkanet.appspot.com",
-        messagingSenderId: "587066110876",
-        appId: "1:587066110876:web:f267941fe51b910686ce59",
-        measurementId: "G-EE686WHS11"
+        apiKey: 'AIzaSyC61BeqwPbtOcofGb1vHvvClo1BaS_rNuY',
+        authDomain: 'zborkanet.firebaseapp.com',
+        databaseURL:
+            'https://zborkanet-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'zborkanet',
+        storageBucket: 'zborkanet.appspot.com',
+        messagingSenderId: '587066110876',
+        appId: '1:587066110876:web:f267941fe51b910686ce59',
+        measurementId: 'G-EE686WHS11'
     }
 };
